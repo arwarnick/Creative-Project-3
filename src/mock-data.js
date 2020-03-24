53709300e1ddbd3b20f0dce6c7979dae9a1a30e9
Austin Warnick <arwarnick@gmail.com>
2020-03-23 22:13:28 -0600

    latest updates

---
+++ src/mock-data.js
@@ -0,0 +1,93 @@
+let mock = [{
+    id: 1,
+    name: "Violin",
+    price: "$200.00",
+    country: "Mexico",
+  },
+  {
+    id: 2,
+    name: "Viola",
+    price: "$150.00",
+    country: "Mexico",
+  },
+  {
+    id: 3,
+    name: "Cello",
+    price: "$500.00",
+    country: "United States",
+  },
+  {
+    id: 4,
+    name: "Bass",
+    price: "$700.00",
+    country: "United States",
+  },
+  {
+    id: 5,
+    name: "Clarinet",
+    price: "$300.00",
+    country: "Canada",
+  },
+  {
+    id: 6,
+    name: "Flute",
+    price: "$400.00",
+    country: "Brazil",
+  },
+  {
+    id: 7,
+    name: "Oboe",
+    price: "$350.00",
+    country: "Brazil",
+  },
+  {
+    id: 8,
+    name: "Bassoon",
+    price: "$250.00",
+    country: "United States",
+  },
+  {
+    id: 9,
+    name: "Trumpet",
+    price: "$400.00",
+    country: "Canada",
+  },
+  {
+    id: 10,
+    name: "Tuba",
+    price: "$700.00",
+    country: "United States",
+  },
+  {
+    id: 11,
+    name: "Trombone",
+    price: "$400.00",
+    country: "United States",
+  },
+  {
+    id: 12,
+    name: "Horn",
+    price: "450.00",
+    country: "Canada",
+  },
+  {
+    id: 13,
+    name: "Timpani",
+    price: "$200.00",
+    country: "Mexico",
+  },
+  {
+    id: 14,
+    name: "Bass Drum",
+    price: "$250.00",
+    country: "Mexico",
+  },
+  {
+    id: 15,
+    name: "Cymbols",
+    price: "$450.00",
+    country: "United States",
+  },
+]
+
+export default mock;
